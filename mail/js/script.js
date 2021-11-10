d5b387691dad65d970e2c28d152cfbc94d6c5eff
francesca <fradeluca85@gmail.com>
2021-11-10 16:27:34 +0100

    container

--- mail/js/script.js
+++ mail/js/script.js
@@ -2,14 +2,25 @@
 
 // alert ('ciao')
 
+const container = document.getElementById('container');
 
 const mailUtente = prompt('Inserisci la tua e-mail');
 // console.log(mailUtente);
 
-const accessList = ["pippo", "pluto", "paperino"];
+    //  controllo se l'utente ha inserito una indirizzo email corretto
+const validazione = /^[_a-z0-9+-]+(\.[_a-z0-9+-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)+$/;
+	if (!validazione.test(mailUtente))
+	{
+	    alert("L'indirizzo email non è valido!");
+	}
+
+    // lista degli indirizzi email che hanno accesso
+const accessList = ["pippo@gmail.com", "pluto@gmail.com", "paperino@gmail.com", "fra@gmail.com"];
+
 
 let sentinella = false;
 
+    // controllo indirizzo email
 for (let i = 0; i < accessList.length; i++){
 
     if(mailUtente == accessList[i]) {sentinella = true;
